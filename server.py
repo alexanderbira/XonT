04af6df62b151d0f799d0649e1a92bb55f178fff
Rukaiya Khondaker <rnk23@ic.ac.uk>
2024-03-09 00:55:52 +0000

    Generalised receiving files into function tackleFiles

--- server.py
+++ server.py
@@ -23,11 +23,10 @@ def form_submission():
     "message": "Success!"
   })
    """
-  if 'style-image' not in request.files:
+  """ if 'style-image' not in request.files:
     return jsonify({"error": "no file part nooo D:"})
   file = request.files['style-image']
-  # If the user does not select a file, the browser submits an
-  # empty file without a filename.
+  
   if file.filename == '':
     return jsonify({"error": "No file submitted nooo D:"})
   
@@ -37,7 +36,35 @@ def form_submission():
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return jsonify({"success": "so win"})
   except Exception as e:
-    return jsonify({"error": str(e)}), 500
+    return jsonify({"error": str(e)}), 500 """
+  return jsonify(tackleFiles("style-image", request.files))
+  
+def tackleFiles(inputFile, requestFiles):
+  if inputFile not in requestFiles:
+    return {
+      "error": "no file part noooo D:",
+      "file": inputFile
+      }
+  file = requestFiles[inputFile]
+
+  if file.filename == "":
+    return {
+      "error": "no file submitted noooo D:"
+      }
+  
+  os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
+  
+  filename = secure_filename(file.filename)
+  try:
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    return {
+      "success": "so win"
+      }
+  except Exception as e:
+    return {
+      "error": str(e),
+      "file": inputFile
+    }
 
 @app.route("/example-api")
 def secret():
