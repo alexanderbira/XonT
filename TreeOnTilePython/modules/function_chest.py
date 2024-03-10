efd0dd5eed07621954af7c104e7e9be9eb712123
LawanF <lawanf@users.noreply.github.com>
2024-03-10 01:17:39 +0000

    removed more useless code

--- TreeOnTilePython/modules/function_chest.py
+++ TreeOnTilePython/modules/function_chest.py
@@ -1,28 +1,14 @@
 import bpy
-import sys
 import os
-# from runScript import tile_path_absolute, source_path_absolute, destination_path_absolute
-from shutil import copy
 from math import radians
-from random import uniform
 
-# src_model_dir = os.path.abspath('images/models')
-# dest_model_dir = os.path.abspath('images/tiles')
-# src_files = ['images/models' + file_name for file_name in os.listdir(src_model_dir)]
-# dest_files = ['images/tiles' + file_name for file_name in os.listdir(src_model_dir)]
-
-tile_path_relative = os.path.abspath('TreeOnTilePython/blender_files/baseTiles/baseTile.obj')
-anyaSrcPath = 'TreeOnTilePython/blender_files/objFromAI/anya.obj'
-anyaDestPath = 'TreeOnTilePython/blender_files/objFromAI/anyaOutput.obj'
-gigaSrcPath = 'TreeOnTilePython/blender_files/objFromAI/gigaLawn.obj'
-gigaDestPath = 'TreeOnTilePython/blender_files/objFromAI/gigaOutput.obj'
-
-
-# test_source = str(input("Enter path to object source: "))
-# test_dest = str(input("Enter path to destination: "))
-
-# test_source = '/Users/lawanfathullah/Documents/TreeOnTilePython/blender_files/anya.obj'
-# test_dest = './blender_files/resultThingie.obj'
+# Loading source paths and destination paths.
+src_model_dir = os.path.abspath('images/models')
+dest_model_dir = os.path.abspath('images/tiles')
+src_paths = ['images/models' + file_name for file_name in os.listdir(src_model_dir)]
+dest_paths = ['images/tiles' + file_name for file_name in os.listdir(src_model_dir)]
+# Path to base tile.
+tile_path = os.path.abspath('TreeOnTilePython/baseTiles/baseTile.obj')
 
 #####################################
 #####################################
@@ -39,14 +25,11 @@ def create_family(family_members):
 
     return parent_obj
 
-# Takes an obj and rotates it and its children in a random angle in the Z axis.
-def randomZRotate(obj):
-    obj.rotation_euler.z = uniform(0, radians(360))
-
 # Takes an obj and finds it minimum and maximum coordinates
 # in the world space. (Accounts for children as well).
-# Returns a tuple of two lists. Minimum and maximum coordinates respectively.
-def getMinMaxWorldCoords(obj):
+# Returns a tuple of two lists. Minimum and maximum coordinates respectively,
+# in the format of [X, Y, Z].
+def get_min_max_world_coords(obj):
     # Get children of obj and put them into a list together.
     family_members = [obj] + list(obj.children)
     # Get all coordinate vectors.
@@ -54,61 +37,65 @@ def getMinMaxWorldCoords(obj):
                      for member in family_members
                      for v in member.data.vertices]
 
-    minCoords = []
-    maxCoords = []
+    min_coords = []
+    max_coords = []
+
+    # Append minimum and maximum coordinates to the arrays.
     for i in range(3):
         vals = [co[i] for co in family_coords]
-        minCoords.append(min(vals))
-        maxCoords.append(max(vals))
+        min_coords.append(min(vals))
+        max_coords.append(max(vals))
 
-    return (minCoords, maxCoords)
+    return (min_coords, max_coords)
 
 # Helper function to scale obj down and up.
-def scalingHelper(obj, scaling_factor):
+def scaling_helper(obj, scaling_factor):
     for index in range(3):
          obj.scale[index] *= scaling_factor
+    # Update matrix.
     bpy.context.view_layer.update()
 
-# Bind X, Y dimension to the ones of binding_obj with a margin.
+
+# Bind X, Y dimensions of obj to the ones of binding_obj with a margin.
 # Also centers the obj onto the coordinates of binding_obj in the X, Y plane.
-# Burrow parameter decides how deep into the binding_obj the object is placed.???
-def placeObjectOn(obj, binding_obj, margin = 0, scaleUp = False):
-    binding_min_coords, binding_max_coords = getMinMaxWorldCoords(binding_obj)
+# Puts the bottom of the obj at the center of binding_obj
+def place_obj_on(obj, binding_obj, margin = 0, scaleUp = False):
+    binding_min_coords, binding_max_coords = get_min_max_world_coords(binding_obj)
     binding_dimensions = [binding_max_coords[i] - binding_min_coords[i] - (2 * margin)
                          for i in range(2)] # Excluding Z coordinate.
-
     binding_center_coords = [(binding_min_coords[i] + binding_max_coords[i]) / 2
                              for i in range(3)]
-    # Add Z coordinate without margins.
     
-    
-    obj_min_coords, obj_max_coords = getMinMaxWorldCoords(obj)
+    obj_min_coords, obj_max_coords = get_min_max_world_coords(obj)
     obj_dimensions = [obj_max_coords[i] - obj_min_coords[i] for i in range(2)]
     
     scaling_factor = min([binding_dimensions[i] / obj_dimensions[i] for i in range(2)])
     if scaling_factor < 1 or scaleUp:
-        scalingHelper(obj, scaling_factor)
-        # Update matrix after scaling.
+        scaling_helper(obj, scaling_factor)
         
+        # Update values of object.
+        obj_min_coords, obj_max_coords = get_min_max_world_coords(obj)
+        obj_center_coords = [(obj_min_coords[i] + obj_max_coords[i]) / 2 for i in range(3)]
 
-    obj_min_coords, obj_max_coords = getMinMaxWorldCoords(obj)
-    obj_dimensions = [obj_max_coords[i] - obj_min_coords[i] for i in range(3)]
-    obj_center_coords = [(obj_min_coords[i] + obj_max_coords[i]) / 2 for i in range(3)]
+    # Center obj on binding_obj with respect to XY plane.
     for i in range(2):
         obj.location[i] -= obj_center_coords[i] - binding_center_coords[i]
 
-    # Adjust Z coordinate without margin.
+    # Adjust Z coordinate separately as obj is put ON TOP of binding_obj.
     obj.location[2] -= (obj_min_coords[2] - binding_center_coords[2])
-    obj_min_coords, obj_max_coords = getMinMaxWorldCoords(obj)
+    obj_min_coords, obj_max_coords = get_min_max_world_coords(obj)
+
+    # Update matrix.
+    bpy.context.view_layer.update()
 
 # Imports the model in source to the copied tile file.
 # Places the model on the square tile, then removes it,
 # leaving only the hexagonal tile and the source object.
 # Exports a .obj file at destination.
-def runPlaceObject(src_file, dest_file):
+def run_place_obj(src_file, dest_file):
     # Then import the tiles.
     # bpy.ops.import_scene.obj(filepath=tile_path_absolute)
-    squareTile = bpy.data.objects['squareTile']
+    square_tile = bpy.data.objects['squareTile']
 
     # Import the 3D model and make them a family.
     bpy.ops.wm.obj_import(filepath=src_file)
@@ -118,17 +105,20 @@ def runPlaceObject(src_file, dest_file):
         parent_obj.rotation_euler.x -= radians(90)
 
         # Place the object on the square tile
-        placeObjectOn(parent_obj, squareTile, 0.2, True)
+        place_obj_on(parent_obj, square_tile, 0.2, True)
 
 
     # Remove the square tile, leaving only the hexagon tile.
-    bpy.data.objects.remove(squareTile)
+    bpy.data.objects.remove(square_tile)
 
     # Export file as obj.
     bpy.ops.wm.obj_export(filepath=dest_file)
 
     # sys.exit()
 
+# Takes a list of source file paths and destination file paths,
+# then generates "X on Tile" at the destination file paths for each
+# source file.
 def generate_objs(src_files, dest_files):
     abs_src_files = [os.path.abspath(file_path) for file_path in src_files]
     abs_dest_files = [os.path.abspath(file_path) for file_path in dest_files]
@@ -136,9 +126,8 @@ def generate_objs(src_files, dest_files):
     for i in range(len(abs_src_files)):
         for obj in bpy.data.objects:
             bpy.data.objects.remove(obj)
-        bpy.ops.wm.obj_import(filepath=tile_path_relative)
+        bpy.ops.wm.obj_import(filepath=tile_path)
     
-        runPlaceObject(abs_src_files[i], abs_dest_files[i])
+        run_place_obj(abs_src_files[i], abs_dest_files[i])
 
-generate_objs([anyaSrcPath, gigaSrcPath], [anyaDestPath, gigaDestPath])
-# generate_objs(src_files, dest_files)
+generate_objs(src_paths, dest_paths)
